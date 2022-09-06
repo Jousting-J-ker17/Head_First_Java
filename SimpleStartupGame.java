fc9af895524ff8685fad7b7ca81723ef30d52069
Jousting-J-ker17 <jlkuhn13@gmail.com>
2022-09-05 21:51:09 -0400

    Add files via upload

---
+++ SimpleStartupGame.java
@@ -0,0 +1,20 @@
+class SimpleStartupGame{
+	public statis void main(String[] args){
+
+
+		class SimpleStartup{
+			private int[] locationCells;
+			private int numOfHits;
+			
+			String checkYourself(int guess){
+			
+
+			}
+
+			void setLocationCells(int[] loc){
+
+			}
+
+		}
+	}
+}
