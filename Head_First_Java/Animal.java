dc89c1589d5568ac211a16f228b10995e0007f4e
Joe Kuhn <jlkuhn13@gmail.com>
2022-09-16 14:19:21 -0400

    updating Animal class

--- Head_First_Java/Animal.java
+++ Head_First_Java/Animal.java
@@ -1,49 +1,49 @@
-public class Animal {
+public abstract class Animal {
 	String food;
 	boolean hunger;
 	int boundaries; 
 	int location;  // Animal HAS-A location
 
-	public String makeNoise();
-	public void eat();
-	public void sleep();
-	public void roam();
+	public abstract String makeNoise(); // Even though an abstract class, not all methods need to be abstract
+	public abstract void eat();
+	public abstract void sleep();
+	public abstract void roam();
 }
 
-public class Feline extends Animal {
+public abstract class Feline extends Animal {
 	public void roam(); // overridden method, inherits everything else from Animal
 }
 
-public class Hippo extends Animal {
+public class Hippo extends Animal { // a concrete class
 	public String MakeNoise(); // overridden methods, inherits everything else from Animal
 	public void roam();
 }
 
-public class Canine extends Animal {
+public abstract class Canine extends Animal {
 	public void roam(); // overridden method, inherits everything else from Animal
 }
 
-public class Lion extends Feline {
+public class Lion extends Feline { // a concrete class
 	public String MakeNoise(); // overridden methods, inherits everything else from Feline and then Animal classes, respectfully
 	public void eat();
 }
 
-public class Tiger extends Feline {
+public class Tiger extends Feline { // a concrete class
 	public String MakeNoise(); // overridden methods, inherits everything else from Feline and then Animal classes, respectfully
 	public void eat();
 }
 
-public class cat extends Feline { 
+public class cat extends Feline {  // a concrete class
 	public String MakeNoise(); // overridden methods, inherits everything else from Feline and then Animal classes, respectfully
 	public void eat();
 }
 
-public class Wolf extends Canine { // Wolf IS-A Animal
+public class Wolf extends Canine { // Wolf IS-A Animal and a concrete class
 	public String MakeNoise(); // overridden methods, inherits everything else from Canine and then Animal classes, respectfully
 	public void eat();
 }
 
-public class Dog extends Canine {
+public class Dog extends Canine { // a concrete class
 	public String MakeNoise(); // overridden methods, inherits everything else from Canine and then Animal classes, respectfully
 	public void eat();
 }
