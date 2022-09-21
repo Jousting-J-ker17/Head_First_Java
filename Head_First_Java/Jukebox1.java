90227d8c62b7beda93911640fcb2289d7c5a1269
Joe Kuhn <jlkuhn13@gmail.com>
2022-09-21 15:22:22 -0400

    Creating a jukebox to display songnames, without sort

---
+++ Head_First_Java/Jukebox1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+
+public class Jukebox1 {
+	public static void main(String []args) {
+		new Jukebox1().go();
+	}
+
+	public void go() {
+		List<String> songList = MockSongs.getSongStrings();
+		System.out.println(songList);
+	}
+}
+
+// standin for I/O code 
+
+class MockSongs {
+	public static List<String> getSongStrings() {
+		List<String> songs = new ArrayList<>();
+		songs.add("somersault");
+		songs.add("cassidy");
+		songs.add("$10");
+		songs.add("havana");
+		songs.add("Cassidy");
+		songs.add("50 Ways");
+		return songs;
+	}
+}
